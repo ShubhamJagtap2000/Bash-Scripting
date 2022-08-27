e323585ab4e5dca667ddc98507da288d963ff4e6
Shubham S Jagtap <63872951+ShubhamJagtap2000@users.noreply.github.com>
2022-08-27 20:23:45 +0530

    Create hang.sh

---
+++ 04 - Parameters/Scripts/hang.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Enter your name
+
+read name
+
+echo "Your name is $name"
+
+# Run this script inputting your name as "Alex" after running using command ./hang.sh in the terminal it will output result with the name you gave as input, otherwise it will hang
