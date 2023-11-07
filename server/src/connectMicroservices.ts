7286e379f76862fc5fe3335cede047afa270ba3b
Shubham S Jagtap <63872951+ShubhamJagtap2000@users.noreply.github.com>
2023-11-07 09:57:11 +0530

    Delete server directory

--- server/src/connectMicroservices.ts
+++
@@ -1,6 +0,0 @@
-import { INestApplication } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-
-export async function connectMicroservices(app: INestApplication) {
-  const configService = app.get(ConfigService);
-}
